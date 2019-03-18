862420bad2a5a3cbf6ec4e1f78ffd4782edf10ed
Łukasz Dramiński <lukasz.draminski@agora.pl>
2019-03-18 19:35:21 +0100

    Now picture is loading when i between anchors

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,68 @@
 
 const scrollingPage = (() => {
 
+    let anchors = document.querySelectorAll('.img-anchor');
+    let assignCordsToAnchors = [];
+    let currentPic = '';
+
     function init() {
 
         console.log('Scrolling Page');
 
-        replaceSrc();
+        //replaceSrc();
+        getAllAnchors(() => {
+            checkWhatImageShouldLoad();
+        });
+
+        document.addEventListener('scroll', ()=>{
+            getAllAnchors(() => {
+                checkWhatImageShouldLoad();
+            });
+        });
+    }
+
+    function checkWhatImageShouldLoad() {
+
+        const imageContainer = document.querySelector('.container-pic');
+
+        let scrollTop = window.pageYOffset + window.innerHeight;
+        let imageSrc = '';
+
+        setTimeout(() => {
+            for(let i = assignCordsToAnchors.length - 1; i >= 0; i--) {
+                if(scrollTop > assignCordsToAnchors[i][0] || scrollTop < assignCordsToAnchors[0][0] ) {
+                    if(currentPic !== assignCordsToAnchors[i][0] ) {
+                        currentPic = assignCordsToAnchors[i][0];
+                        imageSrc = require('../images/' + assignCordsToAnchors[i][1]);
+                        imageContainer.style.cssText = 'background-image: url('+ imageSrc  + ')';
+                        return;
+                    } else {
+                        console.log('Było');
+                        return
+                    }
+                }
+            }
+        }, 200)
+    }
+
+    function getAllAnchors(callback) {
+
+        let running = false;
+
+        if(!running){
+            running = true;
+            setTimeout(()=> {
+                assignCordsToAnchors = [];
+
+                [].forEach.call(anchors, (item)=>{
+                    let cordsX = item.offsetTop;
+                    let imageSrc = item.getAttribute('data-src');
+                    assignCordsToAnchors.push([cordsX, imageSrc]);
+                });
+                running = false;
+                callback();
+            }, 200)
+        }
     }
 
     function replaceSrc() {
